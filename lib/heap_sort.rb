286e3e79c671e5e09a59c95786ec244f386e3bde
stupendousC <CarolineWuKaplan@gmail.com>
2020-03-11 20:14:38 -0700

    finished heap_sort but it's not O(1) space...

--- lib/heap_sort.rb
+++ lib/heap_sort.rb
@@ -1,8 +1,21 @@
-
+require_relative "min_heap"
 
 # This method uses a heap to sort an array.
-# Time Complexity:  ?
-# Space Complexity: ?
-def heap_sort(list)
-  raise NotImplementedError, "Method not implemented yet..."
+# Time Complexity:  O(n logn)
+# Space Complexity: O(n), made a new [] and new MinHeap instance
+def heapsort(list)
+  heap = MinHeap.new()
+  
+  # add each element onto MinHeap
+  list.each do |element|
+    heap.add(element)
+  end
+  
+  # start plucking out the roots from the MinHeap
+  sortedList = []
+  until heap.empty?
+    sortedList << heap.remove()
+  end
+  
+  return sortedList
 end
