c305db4697ec203452d4cea9ba7b42b0708e076a
Amal Hassan-Ali <amalh.a97@gmail.com>
2020-03-12 18:41:10 -0700

    passing tests

--- lib/heap_sort.rb
+++ lib/heap_sort.rb
@@ -1,8 +1,18 @@
 
 
 # This method uses a heap to sort an array.
-# Time Complexity:  ?
-# Space Complexity: ?
-def heap_sort(list)
-  raise NotImplementedError, "Method not implemented yet..."
+# Time Complexity: O(n * log m)?
+# Space Complexity: O(n) ?
+def heapsort(list)
+  sorted_arr = []
+  heap = MinHeap.new()
+  list.each do |value|
+    heap.add(value, value)
+  end 
+
+list.length.times do |i|
+  min = heap.remove()
+  sorted_arr.push(min)
+end 
+return sorted_arr
 end
--- lib/min_heap.rb
+++ lib/min_heap.rb
@@ -14,18 +14,22 @@ class MinHeap
   end
 
   # This method adds a HeapNode instance to the heap
-  # Time Complexity: ?
-  # Space Complexity: ?
+  # Time Complexity: O(log n)
+  # Space Complexity: O(log n)
   def add(key, value = key)
-    raise NotImplementedError, "Method not implemented yet..."
+    @store.push(HeapNode.new(key,value))
+    heap_up(@store.length - 1) if @store.length - 1 != 0 
   end
 
   # This method removes and returns an element from the heap
   #   maintaining the heap structure
-  # Time Complexity: ?
-  # Space Complexity: ?
+  # Time Complexity: O(log n)
+  # Space Complexity: O (log n)
   def remove()
-    raise NotImplementedError, "Method not implemented yet..."
+    swap(0, @store.length - 1)
+    deleted = @store.pop()
+    heap_down(0) unless self.empty?
+    return deleted.value() 
   end
 
 
@@ -44,10 +48,10 @@ class MinHeap
   end
 
   # This method returns true if the heap is empty
-  # Time complexity: ?
-  # Space complexity: ?
+  # Time complexity: O(1)
+  # Space complexity: O(1)
   def empty?
-    raise NotImplementedError, "Method not implemented yet..."
+    return true if @store.empty? 
   end
 
   private
@@ -55,17 +59,45 @@ class MinHeap
   # This helper method takes an index and
   #  moves it up the heap, if it is less than it's parent node.
   #  It could be **very** helpful for the add method.
-  # Time complexity: ?
-  # Space complexity: ?
+  # Time complexity: O (log n)
+  # Space complexity: O(log n)
+  # Formula 
+  # left_child - 2 * i + 1 
+  # right_child - 2 * i + 2 
   def heap_up(index)
-    
+    if index.even?
+      parent_index = (index - 2)/2 
+    elsif index.odd?
+      parent_index = (index - 1)/2 
+    end 
+
+      if @store[index].key < @store[parent_index].key
+        swap(parent_index, index)
+        heap_up(parent_index) if parent_index != 0 
+      end 
+
   end
 
   # This helper method takes an index and 
   #  moves it up the heap if it's smaller
   #  than it's parent node.
   def heap_down(index)
-    raise NotImplementedError, "Method not implemented yet..."
+    return if self.empty?
+    left_child = 2 * index + 1 
+    right_child = 2 * index + 2 
+    if @store[left_child] == nil 
+      return 
+    elsif @store[right_child] == nil || @store[left_child].key < @store[right_child].key
+      smallest_child = left_child 
+    elsif @store[left_child].key > @store[right_child].key
+      smallest_child = right_child 
+    end 
+
+    if @store[index].key > @store[smallest_child].key
+      swap(smallest_child, index)
+      heap_down(smallest_child) 
+    end 
+
   end
 
   # If you want a swap method... you're welcome
