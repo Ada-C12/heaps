e15134d7bbfa2f9ff9313e4c8178048dc27f6da9
Kristina <kristina.tanya@gmail.com>
2020-03-14 15:53:16 -0700

    submitting assignment

--- lib/min_heap.rb
+++ lib/min_heap.rb
@@ -14,18 +14,28 @@ class MinHeap
   end
 
   # This method adds a HeapNode instance to the heap
+  # takes a key & value and constructs a HeapNode to add to the heap
   # Time Complexity: ?
   # Space Complexity: ?
   def add(key, value = key)
-    raise NotImplementedError, "Method not implemented yet..."
+    new_node = HeapNode.new(key, value)
+    @store << new_node
+    heap_up(@store.length-1)
   end
 
   # This method removes and returns an element from the heap
   #   maintaining the heap structure
   # Time Complexity: ?
   # Space Complexity: ?
-  def remove()
-    raise NotImplementedError, "Method not implemented yet..."
+  def remove
+    # Can you delete any other node besides the root?
+    return nil if @store.empty?
+
+    swap(0, @store.length - 1)
+    target = @store.pop
+
+    heap_down(0)
+    return target.value
   end
 
 
@@ -43,11 +53,11 @@ class MinHeap
     return output
   end
 
-  # This method returns true if the heap is empty
+  # This method returns true if the heap is empty; false if not
   # Time complexity: ?
   # Space complexity: ?
   def empty?
-    raise NotImplementedError, "Method not implemented yet..."
+    @store.empty?
   end
 
   private
@@ -57,15 +67,34 @@ class MinHeap
   #  It could be **very** helpful for the add method.
   # Time complexity: ?
   # Space complexity: ?
-  def heap_up(index)
-    
+  def heap_up(new_index)
+    return if new_index == 0
+    parent_index = (new_index - 1) / 2
+
+    if @store[new_index].key < @store[parent_index].key
+      swap(new_index, parent_index)
+      heap_up(parent_index)
+    end
   end
 
   # This helper method takes an index and 
   #  moves it up the heap if it's smaller
   #  than it's parent node.
   def heap_down(index)
-    raise NotImplementedError, "Method not implemented yet..."
+    left_index = index * 2 + 1
+    right_index = index * 2 + 2
+
+    if right_index < @store.length
+      min_child = @store[left_index].key < @store[right_index].key ? left_index : right_index
+      if @store[index].key > @store[min_child].key
+        swap(index, min_child)
+        heap_down(min_child)
+      end
+    elsif left_index < @store.length
+      if @store[index].key > @store[left_index].key
+        swap(index, left_index)
+      end
+    end
   end
 
   # If you want a swap method... you're welcome
--- test/min_heap_test.rb
+++ test/min_heap_test.rb
@@ -53,29 +53,29 @@ describe "Heap" do
 
   it "can remove nodes in the proper order" do
    # Arrange
-   heap.add(3, "Pasta")
-   heap.add(6, "Soup")
-   heap.add(1, "Pizza")
-   heap.add(0, "Donuts")
-   heap.add(16, "Cookies")
-   heap.add(57, "Cake")
+   heap.add(3, 3)
+   heap.add(6, 6)
+   heap.add(1, 1)
+   heap.add(0, 0)
+   heap.add(16, 16)
+   heap.add(57, 57)
 
    # Act
    removed = heap.remove
 
    # Assert
-   expect(removed).must_equal "Donuts"
+   expect(removed).must_equal 0
 
    # Another Act
    removed = heap.remove
 
    # Another assert
-   expect(removed).must_equal "Pizza"
+   expect(removed).must_equal 1
 
   # Another Act
   removed = heap.remove
 
   # Another assert
-  expect(removed).must_equal "Pasta"
+  expect(removed).must_equal 3
   end
 end
