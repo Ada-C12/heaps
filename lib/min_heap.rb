ed08b2e9fca975fc6215a8dc1f8d9d52a5d3b712
Emily Vomacka <emilyvomacka@gmail.com>
2020-03-17 11:16:55 -0700

    complete

--- lib/min_heap.rb
+++ lib/min_heap.rb
@@ -1,6 +1,6 @@
 class HeapNode
   attr_reader :key, :value
-
+  
   def initialize(key, value)
     @key = key
     @value = value
@@ -8,66 +8,93 @@ class HeapNode
 end
 
 class MinHeap
-
+  
   def initialize
     @store = []
   end
-
+  
   # This method adds a HeapNode instance to the heap
-  # Time Complexity: ?
-  # Space Complexity: ?
+  # Time Complexity: O(log n)
+  # Space Complexity: O(1)
   def add(key, value = key)
-    raise NotImplementedError, "Method not implemented yet..."
+    @store << HeapNode.new(key, value)
+    heap_up(@store.length - 1)
   end
-
+  
   # This method removes and returns an element from the heap
   #   maintaining the heap structure
-  # Time Complexity: ?
-  # Space Complexity: ?
+  # Time Complexity: O(log n)
+  # Space Complexity: O(1)
   def remove()
-    raise NotImplementedError, "Method not implemented yet..."
+    swap(0, @store.length - 1)
+    remove_node = @store.pop
+    heap_down(0)
+    return remove_node.value
   end
-
-
+  
+  
   # Used for Testing
   def to_s
     return "[]" if @store.empty?
-
+    
     output = "["
     (@store.length - 1).times do |index|
       output += @store[index].value + ", "
     end
-
+    
     output += @store.last.value + "]"
-      
+    
     return output
   end
-
+  
   # This method returns true if the heap is empty
-  # Time complexity: ?
-  # Space complexity: ?
+  # Time complexity: O(1)
+  # Space complexity: O(1)
   def empty?
-    raise NotImplementedError, "Method not implemented yet..."
+    return @store.empty?
   end
-
+  
   private
-
+  
   # This helper method takes an index and
-  #  moves it up the heap, if it is less than it's parent node.
+  #  moves it up the heap, if it is less than its parent node.
   #  It could be **very** helpful for the add method.
-  # Time complexity: ?
-  # Space complexity: ?
+  # Time complexity: O(log n)
+  # Space complexity: O(1)
   def heap_up(index)
-    
+    while index > 0 && @store[(index - 1) / 2].key > @store[index].key
+      swap(((index - 1) / 2), index)
+      index = (index - 1) / 2
+    end
   end
-
-  # This helper method takes an index and 
-  #  moves it up the heap if it's smaller
-  #  than it's parent node.
+  
+  # This helper method takes an index and moves it down the heap if it's smaller than its child node.
   def heap_down(index)
-    raise NotImplementedError, "Method not implemented yet..."
-  end
-
+    while !is_leaf(index)
+      if @store[index].key > @store[left_child(index)].key || @store[index].key > @store[right_child(index)].key
+        if @store[left_child(index)].key < @store[right_child(index)].key
+          swap(index, left_child(index))
+          index = left_child(index)
+        else
+          swap(index, right_child(index))
+          index = right_child(index)
+        end 
+      end 
+    end
+  end 
+  
+  def is_leaf(index)
+    return index >= ((@store.length - 1) / 2) && index < @store.length
+  end 
+  
+  def left_child(index)
+    return index * 2 + 1
+  end 
+  
+  def right_child(index)
+    return index * 2 + 2
+  end 
+  
   # If you want a swap method... you're welcome
   def swap(index_1, index_2)
     temp = @store[index_1]
