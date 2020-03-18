9215ac9dac848d2e6bc9560784abfb6a1fc0d9d7
Nicky Choi <nickyjinchoi@gmail.com>
2020-03-18 16:50:06 -0700

    Unskipped tests

--- test/heapsort_test.rb
+++ test/heapsort_test.rb
@@ -1,24 +1,24 @@
 require_relative "test_helper"
 
-xdescribe "heapsort" do
+describe "heapsort" do
   it "sorts an empty array" do
     # Arrange 
     list = []
-
+    
     # Act
-    result = heapsort(list)
-
+    result = heap_sort(list)
+    
     # Assert
     expect(result).must_equal []
   end
-
+  
   it "can sort a 1-element array" do
     # Arrange 
     list = [5]
-
+    
     # Act
-    result = heapsort(list)
-
+    result = heap_sort(list)
+    
     # Assert
     expect(result).must_equal [5]
   end
@@ -26,10 +26,10 @@ xdescribe "heapsort" do
   it "can sort a 5-element array" do
     # Arrange 
     list = [5, 27, 3, 16, -50]
-
+    
     # Act
-    result = heapsort(list)
-
+    result = heap_sort(list)
+    
     # Assert
     expect(result).must_equal [-50, 3, 5, 16, 27]
   end  
