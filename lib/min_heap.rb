07f1f7717e540ca1dc3f1a60d5fa6e0f52214879
Dominique <dom_taylor@icloud.com>
2020-03-17 19:28:05 -0700

    most recent commit

--- lib/min_heap.rb
+++ lib/min_heap.rb
@@ -14,8 +14,9 @@ class MinHeap
   end
 
   # This method adds a HeapNode instance to the heap
-  # Time Complexity: ?
-  # Space Complexity: ?
+  # Time Complexity: (logn) because in the worst case an element inserted at the bottom has to be swapped at 
+  # every level from bottom to top up to the root node to maintain the heap property. So essentially you are only traversing half the number of the heap.
+  # Space Complexity: O(1) because there will always be one space of memory allocated for a new heap node 
   def add(key, value = key)
     if @store.length == 0
       @store << HeapNode.new(key, value)
@@ -23,17 +24,24 @@ class MinHeap
     end
 
     @store << HeapNode.new(key, value)
-    # heap_up(@store.length - 1)
     heap_up(@store.length - 1)
     
   end
 
   # This method removes and returns an element from the heap
   #   maintaining the heap structure
-  # Time Complexity: ?
-  # Space Complexity: ?
+  # Time Complexity: (logn) because after the swapping of the root node and the last node, the last node may have to traverse the tree height (heap down)
+  # to satisfy the heap
+  # Space Complexity: O(1) because only one space in memory will be created as the algorithm scales.
   def remove()
-    raise NotImplementedError, "Method not implemented yet..."
+
+    return "This heap is empty" if @store.empty?
+
+    swap(0, @store.length - 1)
+    result = @store.pop
+
+    heap_down(0)
+    return result.value
   end
 
 
@@ -52,10 +60,15 @@ class MinHeap
   end
 
   # This method returns true if the heap is empty
-  # Time complexity: ?
-  # Space complexity: ?
+  # Time complexity: O(1) because the algorithm is only checking to see if there are elements in the array. This
+  # will stay as the problem scales
+  # Space complexity: O(1) because no new memory will be created as the algorithm scales
   def empty?
-    raise NotImplementedError, "Method not implemented yet..."
+    if @store[0]
+      return
+    else
+      return false
+    end 
   end
 
   private
@@ -84,8 +97,25 @@ class MinHeap
   # This helper method takes an index and 
   #  moves it up the heap if it's smaller
   #  than it's parent node.
-  def heap_down(index)
-    raise NotImplementedError, "Method not implemented yet..."
+  def heap_down(current_index)
+    # can there be duplicate values in a heap?
+
+    left_child_index = current_index * 2 + 1
+    right_child_index = current_index * 2 + 2
+
+    while @store[left_child_index] || @store[right_child_index] 
+      # having trouble figuring out when to end the while loop
+      if @store[left_child_index].key < @store[right_child_index].key
+        swap(current_index, left_child_index)
+        current_index = left_child_index
+      else 
+        swap(current_index, right_child_index)
+        current_index = right_child_index
+      end 
+      left_child_index = current_index * 2 + 1
+      right_child_index = current_index * 2 + 2
+    end
+    
   end
 
   # If you want a swap method... you're welcome
@@ -96,12 +126,15 @@ class MinHeap
   end
 end
 
-# heap = MinHeap.new
+heap = MinHeap.new
 #  heap.add(3, "Pasta")
 # print heap.add(2, "Soup")
-# heap.add(3, "Pasta")
-# heap.add(6, "Soup")
-# heap.add(1, "Pizza")
-# heap.add(0, "Donuts")
-# heap.add(16, "Cookies")
-# heap.add(57, "Cake")
+heap.add(3, "Pasta")
+heap.add(6, "Soup")
+heap.add(1, "Pizza")
+heap.add(0, "Donuts")
+heap.add(16, "Cookies")
+heap.add(57, "Cake")
+
+print heap
+
