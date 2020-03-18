f66a216e831f5ce196418e84d4ebd048113932bf
Julia A Kingrey <juliakingreyworks@gmail.com>
2020-03-18 15:07:35 -0700

    Passing heapsort tests

--- lib/heap_sort.rb
+++ lib/heap_sort.rb
@@ -1,8 +1,27 @@
-
-
 # This method uses a heap to sort an array.
-# Time Complexity:  ?
-# Space Complexity: ?
-def heap_sort(list)
-  raise NotImplementedError, "Method not implemented yet..."
+# Time Complexity: n log n -- n to put elements in heap, log n to sort, 
+# and n log n put them back in the array
+# Space Complexity: O(n)
+def heapsort(list)
+  return [] if list.length == 0
+  return list if list.length == 1
+
+  heap = MinHeap.new
+  sorted_list = []
+
+  # put each list element in a min heap
+  # O of n operation where n is the number of elements in the list
+  list.each do |element|
+    # log n operation
+    heap.add(element)
+  end
+
+  # remove elements from the min heap (will always remove lowest-value key)
+  # and put them into the sorted list
+  list.length.times do
+    # log n operation
+    sorted_list << heap.remove
+  end
+
+  return sorted_list
 end
