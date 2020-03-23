b3d9a941e814a6a12ce5f689211a11dc82671b42
Angele Zamarron <angelez@allenai.org>
2020-03-22 18:49:04 -0700

    time/space complexity for heapsort

--- lib/heap_sort.rb
+++ lib/heap_sort.rb
@@ -1,8 +1,8 @@
 require_relative 'min_heap'
 
 # This method uses a heap to sort an array.
-# Time Complexity:  ?
-# Space Complexity: ?
+# Time Complexity:  o((n * log n) + (n * log n)) => o(n log n) | it depends on the length of the list, but adding and removing from the list takes log(n), and the removal of each takes log(n) as well, and has to happen n times (the length of the list)
+# Space Complexity: also o(n log n)
 def heapsort(list)
   return list if list.empty?
   sorting_heap = MinHeap.new
