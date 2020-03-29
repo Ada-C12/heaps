3d21a82e28385ce2d26029f81d179c7db0690192
Ga-Young Jin <gyjinn@gmail.com>
2020-03-28 21:34:55 -0700

    added a comment on heapsort

--- lib/heap_sort.rb
+++ lib/heap_sort.rb
@@ -18,3 +18,5 @@ def heap_sort(list)
   
   return list
 end
+
+# I'm having trouble figuring out why the "can sort a 5-element array" test will not pass for this. I have created a new "can sort a different 5-element array" test and that passes. I am thinking it has to do with the order of numbers - some exact pattern of numbers is causing the heap to not sort properly, but all my tests from the min_heap are passing. Could I get some help?
