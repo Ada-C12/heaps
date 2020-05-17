fbf5dc819897cf396b3994edb5fcbdd2562e8d49
Rinostar <codingrinostar@gmail.com>
2020-05-16 21:25:10 -0700

    add runtime

--- lib/heap_sort.rb
+++ lib/heap_sort.rb
@@ -1,7 +1,7 @@
 require_relative 'min_heap'
 # This method uses a heap to sort an array.
-# Time Complexity:  ?
-# Space Complexity: ?
+# Time Complexity:  O(log n)
+# Space Complexity: O(n)
 def heapsort(list)
   return list if list.length <= 1
 
